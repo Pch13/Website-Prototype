b2fc1dc1febb48aa2b2ec05c35289be05167ab83
Pch13 <momokamurata.sub@gmail.com>
2025-05-16 16:04:38 +0900

    second commit added webgl coding for a square

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // might be needed for Lint
 // #pragma vscode_glsllint_stage vert;
+import { initBuffers  } from "./int-buffers.js";
+import { drawScene } from "./draw-scenes.js";
 
 main();
 
@@ -13,7 +15,36 @@ const shaderProgram = gl.createProgram();
 gl.attachShader(shaderProgram, vertexShader);
 gl.attachShader(shaderProgram, fragmentShader);
 gl.linkProgram(shaderProgram);
+
+if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
+    alert(
+        `Unable to initialize the shader proram: $(gl.getProgramInfoLog(shaderProgram,
+        ))`,
+    );
+    return null;
+    }
+
+    return shaderProgram;
 }
+
+function loadShader(gl, type, source){
+    const shader = gl.createShader(type);
+    
+    gl.shaderSource(shader, source);
+
+    gl.compileShader(shader);
+
+if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
+    alert(
+        `An error occured compilind the shaders: $(gl.getShaderInfoLog(shader))`,
+    );
+    gl.deleteShader(shader);
+    return null;
+    }
+    
+    return shader;
+}
+
 
 function main(){
     const canvas = document.querySelector("#canvas");
@@ -46,4 +77,21 @@ function main(){
 
     `;
         //gl_FragColor is a variable with a "float4"
+
+    const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
+
+    const programInfo={
+    program: shaderProgram,
+    attribLocations:{
+        vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+    },
+    uniformLocations:{
+        projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+        modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+    },
+};
+
+    const buffers = initBuffers(gl);
+
+    drawScene(gl, programInfo, buffers);
 }
