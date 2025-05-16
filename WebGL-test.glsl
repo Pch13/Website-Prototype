eaa530d08a556f77212f0892148afcead9e85da9
Pch13 <momokamurata.sub@gmail.com>
2025-05-16 17:32:27 +0900

    *追記

--- WebGL-test.glsl
+++ WebGL-test.glsl
@@ -12,3 +12,7 @@ void main(){
 
     gl_FragColor = vec4(vec3(st.x, st.y, sin(u_time)), 1.0);
 }
+
+
+//VSCode内のglslCanvas エクステンションでプレビューできます
+//参考サイト：https://qiita.com/doxas/items/b8221e92a2bfdc6fc211
