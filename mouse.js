95a681f4c42f8c886b32087c6e0a01f523683ba9
Pch13 <momokamurata.sub@gmail.com>
2025-06-02 16:42:28 +0900

    メインのスタイリングを改良しました リスティングの表示、トランジション機能などを追加

--- mouse.js
+++ mouse.js
@@ -1,16 +1,15 @@
-//マウス追従
 document.addEventListener("DOMContentLoaded", function(){
     const stalker = document.getElementById("cursor-stalker");
     document.addEventListener("mousemove", function(e){
         const x = e.clientX;
         const y = e.clientY;
-        //リンクに反応する
         const links = document.querySelectorAll("a");
 
+        //マウス追従
         stalker.style.opacity = ".6";
-
         stalker.style.transform = "translate(" + x + "px, " + y +"px)";
 
+        //リンクに反応する
         links.forEach(function(link){
             link.addEventListener("mouseenter", function(){
                 stalker.classList.add("cursor-hover");
